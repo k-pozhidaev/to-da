ea0be45c948a5e34b558bc0d4249fec7b25adf3f
Pozhidaev Konstantin <k.pozhidaev@sap.com>
2020-10-10 23:16:11 +0300

    update: - topics HQL to sql - topic search

--- src/main/java/io/poshidaev/toda/controller/TopicController.java
+++ src/main/java/io/poshidaev/toda/controller/TopicController.java
@@ -4,11 +4,15 @@ package io.poshidaev.toda.controller;
 import io.poshidaev.toda.dto.TopicDTO;
 import io.poshidaev.toda.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/topic")
@@ -27,12 +31,12 @@ public class TopicController {
         this.jdbcScheduler = jdbcScheduler;
     }
 
-    @GetMapping(value = {"/top", "/top/:text"})
-    public Flux<TopicDTO> getTopTopics(){
+    @GetMapping(value = {"/top", "/top/{text}"})
+    public Flux<TopicDTO> getTopTopics(@PathVariable Optional<String> text){
         return Flux.defer( () ->
-                Flux.fromIterable(topicRepository.getPopularTen())
+                Flux.fromIterable(topicRepository.getPopular(text.map(s -> s + "%").orElse("%"), PageRequest.of(0, 10)))
                         .subscribeOn(jdbcScheduler)
-                        .map(TopicDTO::new)
+                        .map(topic -> new TopicDTO(topic.getText()))
         );
     }
 }
--- src/main/java/io/poshidaev/toda/repository/TopicRepository.java
+++ src/main/java/io/poshidaev/toda/repository/TopicRepository.java
@@ -1,19 +1,23 @@
 package io.poshidaev.toda.repository;
 
 import io.poshidaev.toda.entity.Topic;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic, String> {
 
     @Query(value = """
-SELECT t.text
-FROM topic t
-left join tasks_to_topics ttt on t.text = ttt.topic_id
+SELECT t
+FROM Topic t
+left join t.goals
+where t.text like :text
 group by t.text
-order by count(ttt.topic_id) DESC
-""", nativeQuery = true)
-    List<String> getPopularTen();
+order by size(t.goals) DESC
+""")
+    List<Topic> getPopular(@Param("text") String text, Pageable pageable);
+
 }
