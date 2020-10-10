3eb46af93968967b742da62a0284e911effee366
Pozhidaev Konstantin <k.pozhidaev@sap.com>
2020-10-10 22:39:47 +0300

    add topics top

--- src/main/java/io/poshidaev/toda/controller/TopicController.java
+++ src/main/java/io/poshidaev/toda/controller/TopicController.java
@@ -1,10 +1,38 @@
 package io.poshidaev.toda.controller;
 
 
+import io.poshidaev.toda.dto.TopicDTO;
+import io.poshidaev.toda.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
 
 @RestController
 @RequestMapping("/api/topic")
 public class TopicController {
+
+    private TopicRepository topicRepository;
+    private Scheduler jdbcScheduler;
+
+    @Autowired
+    public void setTopicRepository(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
+    @Autowired
+    public void setJdbcScheduler(Scheduler jdbcScheduler) {
+        this.jdbcScheduler = jdbcScheduler;
+    }
+
+    @GetMapping(value = {"/top", "/top/:text"})
+    public Flux<TopicDTO> getTopTopics(){
+        return Flux.defer( () ->
+                Flux.fromIterable(topicRepository.getPopularTen())
+                        .subscribeOn(jdbcScheduler)
+                        .map(TopicDTO::new)
+        );
+    }
 }
--- src/main/java/io/poshidaev/toda/repository/TopicRepository.java
+++ src/main/java/io/poshidaev/toda/repository/TopicRepository.java
@@ -2,6 +2,18 @@ package io.poshidaev.toda.repository;
 
 import io.poshidaev.toda.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic, String> {
+
+    @Query(value = """
+SELECT t.text
+FROM topic t
+left join tasks_to_topics ttt on t.text = ttt.topic_id
+group by t.text
+order by count(ttt.topic_id) DESC
+""", nativeQuery = true)
+    List<String> getPopularTen();
 }
